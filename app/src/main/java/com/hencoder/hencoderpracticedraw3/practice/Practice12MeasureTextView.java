3dd12e69e1d55bec94fe33ae7c90984f4b4bf745
diankun.qiu <diankun.qiu@ecovacs.com>
2017-10-19 17:31:53 +0800

    add draw text demo

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice12MeasureTextView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice12MeasureTextView.java
@@ -40,7 +40,9 @@ public class Practice12MeasureTextView extends View {
         // 使用 Paint.measureText 测量出文字宽度，让文字可以相邻绘制
 
         canvas.drawText(text1, 50, 200, paint1);
-        canvas.drawText(text2, 50 + 100, 200, paint2);
-        canvas.drawText(text3, 50 + 200, 200, paint1);
+        float textWidth1 = paint1.measureText(text1, 0, text1.length());
+        canvas.drawText(text2, 50 + textWidth1, 200, paint2);
+        float textWidth2 = paint2.measureText(text2, 0, text2.length());
+        canvas.drawText(text3, 50 + textWidth1 + textWidth2, 200, paint1);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -37,7 +38,18 @@ public class Practice13GetTextBoundsView extends View {
         paint1.setStrokeWidth(20);
         paint1.setColor(Color.parseColor("#E91E63"));
         paint2.setTextSize(160);
+
+        stringBuffer.append(text1)
+                .append(text2)
+                .append(text3)
+                .append(text4)
+                .append(text5)
+                .append(text6);
     }
+
+    static StringBuffer stringBuffer = new StringBuffer();
+    Rect bound = new Rect();
+
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -49,12 +61,19 @@ public class Practice13GetTextBoundsView extends View {
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
 
-        int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+        String text = stringBuffer.toString();
+        paint2.getTextBounds(text, 0, text1.length(), bound);
+
+        float textY = top + ((bottom - top) - bound.height()) / 2.0f + bound.height();
+
+        canvas.drawText(text, 100, textY, paint2);
+
+//        int middle = (top + bottom) / 2;
+//        canvas.drawText(text1, 100, middle, paint2);
+//        canvas.drawText(text2, 200, middle, paint2);
+//        canvas.drawText(text3, 300, middle, paint2);
+//        canvas.drawText(text4, 400, middle, paint2);
+//        canvas.drawText(text5, 500, middle, paint2);
+//        canvas.drawText(text6, 600, middle, paint2);
     }
 }
